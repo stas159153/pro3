7a759bc5522d56d78ea607ea2023601c505b8bdc
STas <108541890+stas159153@users.noreply.github.com>
2023-10-08 23:04:09 +0300

    Add files via upload

---
+++ game4.js
@@ -0,0 +1,46 @@
+function fd() {
+    const g4box = document.querySelector('.game-4__box')
+    const g4conteiner = document.querySelector('.game-4__conteiner')
+    let adaptive = 270
+    if(Number(window.innerWidth) + 0 <= 833 ){
+        adaptive = 500
+        console.log(window.innerWidth);
+    }
+    if(Number(window.innerWidth) + 0 <= 610) {
+        adaptive = 270 
+        console.log(window.innerWidth);
+    }
+    g4conteiner.style.width = adaptive +'px'
+            g4conteiner.addEventListener('click',event => {
+        let a
+        let b
+        if (!window.screenLeft && adaptive >= 720 ) {
+            a = event.screenLeft = event.screenX;
+             b = event.screenTop = event.screenY;
+          g4box.style.left = a - 615 + 'px'
+          g4box.style.top = b - 790 + 'px'
+          console.log(a,b)
+        }
+        
+      else if (!window.screenLeft && adaptive >= 500 ) {
+            a = event.screenLeft = event.screenX;
+             b = event.screenTop = event.screenY;
+          
+          g4box.style.left = a - 425 + 'px'
+          g4box.style.top = b - 750 + 'px'
+          console.log(a,b)
+      }
+
+      else if (!window.screenLeft && adaptive >= 270 ) {
+        a = event.screenLeft = event.screenX;
+         b = event.screenTop = event.screenY;
+      
+      g4box.style.left = a - 840 + 'px'
+      g4box.style.top = b - 790 + 'px'
+      console.log(a,b)
+  }
+        })
+    }
+
+
+fd()
